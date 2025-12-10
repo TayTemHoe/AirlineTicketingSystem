a02a65a50ccb1b68fd4eb0a3b6009a81b5e23529
TayHoem1604 <137590299+TayTemHoe@users.noreply.github.com>
2025-12-10 16:52:02 +0800

    Start

---
+++ AirlineTicketingSystem/src/main/java/com/mycompany/airlineticketingsystem/AirlineTicketingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlineticketingsystem;
+
+/**
+ *
+ * @author Tay Tem Hoe
+ */
+public class AirlineTicketingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
