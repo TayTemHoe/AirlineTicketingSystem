19368e8f0b3410d99c8af98cae640860390ef29a
TayHoem1604 <137590299+TayTemHoe@users.noreply.github.com>
2025-12-11 10:12:25 +0800

    default update

--- AirlineTicketingSystem/src/main/java/com/mycompany/airlineticketingsystem/AirlineTicketingSystem.java
+++ AirlineTicketingSystem/src/main/java/com/mycompany/airlineticketingsystem/AirlineTicketingSystem.java
@@ -8,9 +8,39 @@ package com.mycompany.airlineticketingsystem;
  *
  * @author Tay Tem Hoe
  */
-public class AirlineTicketingSystem {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class AirlineTicketingSystem extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Load the Login.fxml file we just created
+        scene = new Scene(loadFXML("Login"), 640, 480);
+        stage.setScene(scene);
+        stage.setTitle("Airline Modernisation Project");
+        stage.show();
+    }
+
+    // Helper method to switch scenes (You will use this a lot!)
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AirlineTicketingSystem.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void start() {
+        launch();
     }
 }
